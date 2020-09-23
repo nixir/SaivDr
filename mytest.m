e68619c5791352554d2da5d9bd462b081a38d013
Yuya KODAMA <ykodama@ieee.org>
2020-09-23 21:58:34 +0900

    TestRunner to parallelize the tests. Serial tests take 2494 seconds. Parallel tests take 1390 seconds.

--- mytest.m
+++ mytest.m
@@ -1,7 +1,7 @@
 function testRes = mytest(testCaseStr,isProfiling)
 %MYTEST Script of unit testing for SaivDr Package
 %
-% This test script works with unit testing framework 
+% This test script works with unit testing framework
 % See the following site:
 %
 % http://www.mathworks.co.jp/jp/help/matlab/matlab-unit-test-framework.html
@@ -19,19 +19,19 @@ function testRes = mytest(testCaseStr,isProfiling)
 %
 % http://msiplab.eng.niigata-u.ac.jp/
 %
-
+tic
 %%
 import matlab.unittest.TestSuite
-
+import matlab.unittest.TestRunner
 if nargin > 0
     isTestCase = true;
 else
     clear classes %#ok
-    isTestCase = false;
+isTestCase = false;
 end
 
 if nargin < 2
-    isProfiling = false;
+isProfiling = false;
 end
 
 %% Package list
@@ -43,21 +43,24 @@ packageList = { ...
     'saivdr.testcase.dictionary.nsoltx.design',...
     'saivdr.testcase.dictionary.olaols',...
     'saivdr.testcase.dictionary.nsgenlotx',...
-    'saivdr.testcase.dictionary.nsgenlotx.design',...    
+    'saivdr.testcase.dictionary.nsgenlotx.design',...
     'saivdr.testcase.dictionary.generalfb',...
     'saivdr.testcase.dictionary.mixture',...
     'saivdr.testcase.dictionary.utility',...
     'saivdr.testcase.utility',...
-    'saivdr.testcase.sparserep',...
     'saivdr.testcase.degradation',...
-    'saivdr.testcase.degradation.noiseprocess',...
+    'saivdr.testcase.degradation.noiseprocess'...
+    };
+
+packageList_serial = {...
+    'saivdr.testcase.sparserep',...
     'saivdr.testcase.degradation.linearprocess',...
     'saivdr.testcase.restoration.ista',...
-    'saivdr.testcase.restoration.pds',...    
-    'saivdr.testcase.restoration.denoiser',...        
-    'saivdr.testcase.restoration.metricproj',...            
+    'saivdr.testcase.restoration.pds',...
+    'saivdr.testcase.restoration.denoiser',...
+    'saivdr.testcase.restoration.metricproj',...
     'saivdr.testcase.embedded'...
-    };
+    };%parpool function cannot be executed by parpool worker.
 
 %% Set path
 setpath
@@ -70,6 +73,7 @@ if isTestCase
     testCase = eval(testCaseStr);
     testRes = run(testCase);
 else
+    packageList = [packageList, packageList_serial];
     testRes = cell(length(packageList),2);
     for idx = 1:length(packageList)
         if verLessThan('matlab','8.2.0.701') && ...
@@ -77,8 +81,14 @@ else
             disp('Package +embedded is available for R2013b or later.')
         else
             packageSuite = TestSuite.fromPackage(packageList{idx});
+            runner = TestRunner.withTextOutput;
+            
             testRes{idx,1} = packageList{idx};
-            testRes{idx,2} = run(packageSuite);
+            if idx > length(packageList) - length(packageList_serial)
+                testRes{idx,2} = run(packageSuite);
+            else
+                testRes{idx,2} = runInParallel(runner,packageSuite);
+            end
         end
     end
 end
@@ -89,4 +99,3 @@ end
 
 %% License check
 license('inuse')
-
